214ac68923a7c08ff7d8be50f98093d1cd842d24
edpe <ed@edperkins.co.uk>
2021-06-28 09:30:00 +0100

    Revert "feat: AWS config"
    This reverts commit c086b20d52e3e390d8ca854a1db269e6c2bcf77c.

--- config/database.js
+++ config/database.js
@@ -1,18 +1,14 @@
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
+  defaultConnection: 'default',
   connections: {
     default: {
-      connector: "bookshelf",
+      connector: 'bookshelf',
       settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", ""),
-        password: env("DATABASE_PASSWORD", ""),
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
-        ssl: false,
+        useNullAsDefault: true,
       },
     },
   },
